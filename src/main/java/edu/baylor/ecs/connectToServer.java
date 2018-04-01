4a140c9d0a7bcddebfd9155828d85de585d5a348
Mario Lopez <Mario_Lopez@Baylor.edu>
2018-04-01 00:34:37 -0500

    Added RDS instance information to connectToServer. YAY!

--- src/main/java/edu/baylor/ecs/connectToServer.java
+++ src/main/java/edu/baylor/ecs/connectToServer.java
@@ -4,13 +4,12 @@ import java.sql.*;
 
 public class connectToServer {
     private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
-    private static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/tictacclaw;";
-    private static final String DB_USER = "root";
+    private static final String DB_CONNECTION = "jdbc:mysql://tictacclawinstance.ckgzixjdcowi.us-east-2.rds.amazonaws.com:3306/tictacclaw?autoReconnect=true&useSSL=false";
+    private static final String DB_USER = "mario";
     private static final String DB_PASSWORD = "Gibson123";
 
     public static void main(String[] argv) {
         try {
-
             query();
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -21,8 +20,8 @@ public class connectToServer {
 
     private static void query() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection dbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tictacclaw?autoReconnect=true&useSSL=false","root","Gibson123");
+            Class.forName(DB_DRIVER);
+            Connection dbConnection = DriverManager.getConnection(DB_CONNECTION,DB_USER, DB_PASSWORD);
 
             Statement statement = dbConnection.createStatement();
             ResultSet rs = statement.executeQuery("select * from users");
@@ -30,7 +29,7 @@ public class connectToServer {
             while(rs.next()) {
                 System.out.println(
                         "usr_ID: " + rs.getInt(1) + "\n" +
-                        "Username: " + rs.getString(2) + "\n" +
+                        "usr_Name: " + rs.getString(2) + "\n" +
                         "usr_Password: " + rs.getString(3) + "\n" +
                         "usr_HighScore: " + rs.getInt(4) + "\n" +
                         "usr_ImageIndex: " + rs.getInt(5));
