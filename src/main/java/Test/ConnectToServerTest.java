847d9e3be22cbc1e857cf9a8bbae450d18809411
Unknown <Mario_Lopez@Baylor.edu>
2018-04-26 19:25:58 -0500

    Editing pom for JUnit tests. Creating tests for ConnectToServer.

---
+++ src/main/java/Test/ConnectToServerTest.java
@@ -0,0 +1,26 @@
+package Test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectToServerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void login() {
+    }
+}
